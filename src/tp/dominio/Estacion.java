a71368f0f0cf02ef672695ec656a6d1e55dff0ba
ThePunisherCodes <mauro.aa.1102@gmail.com>
2021-07-13 18:32:51 -0300

    first commit

---
+++ src/tp/dominio/Estacion.java
@@ -0,0 +1,56 @@
+package tp.dominio;
+
+import java.time.LocalTime;
+
+public class Estacion {
+	Integer id;
+	String nombre;
+	LocalTime horarioApertura;
+	LocalTime horarioCierre;
+	String estado;
+	
+	//Constructor
+	public Estacion(Integer id, String nombre, LocalTime horarioApertura, LocalTime horarioCierre, String estado) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.horarioApertura = horarioApertura;
+		this.horarioCierre = horarioCierre;
+		this.estado = estado;
+	}
+	
+	
+	//Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public LocalTime getHorarioApertura() {
+		return horarioApertura;
+	}
+	public void setHorarioApertura(LocalTime horarioApertura) {
+		this.horarioApertura = horarioApertura;
+	}
+	public LocalTime getHorarioCierre() {
+		return horarioCierre;
+	}
+	public void setHorarioCierre(LocalTime horarioCierre) {
+		this.horarioCierre = horarioCierre;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+}
