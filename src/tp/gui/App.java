9656dbab8a1996368839cb09fc58fa2b4cf5a1aa
ThePunisherCodes <mauro.aa.1102@gmail.com>
2021-08-04 23:18:01 -0300

    Boletines

--- src/tp/gui/App.java
+++ src/tp/gui/App.java
@@ -16,14 +16,21 @@ public class App extends JFrame {
 		JPanel menu = new JPanel();
 		
 		JButton boton = new JButton("Estacion");
+		JButton boton2 = new JButton("Boleto");
 		JButton boton3 = new JButton("Linea");
 		
 
 		menu.add(boton);
-		menu.add(boton3);
+		menu.add(boton2);
+ 		menu.add(boton3);
 		
 		boton.addActionListener(e -> {
 			EstacionGUI eg = new EstacionGUI();
+			eg.setVisible(true);
+		});
+		
+		boton2.addActionListener(e -> {
+			BoletoGUI eg = new BoletoGUI();
 			eg.setVisible(true);
 		});
 		
